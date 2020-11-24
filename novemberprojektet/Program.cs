4db300bb4fcfd5436ceadb4545fd60cd964f6ad1
ertkonsert <martaaagren@gmail.com>
2020-11-24 10:08:10 +0100

    first commit

---
+++ novemberprojektet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Raylib_cs;
+
+namespace novemberprojektet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadLine();
+        }
+    }
+}
